a27c08e5790ed3730ec8918c06082c8ab32b0fab
Anisur-Rahman-Rakib <amianis1234567890@gmail.com>
2020-08-16 23:58:55 +0600

    somthing bug solving

--- app.js
+++ app.js
@@ -10,13 +10,22 @@ let descriptionText = document.getElementById("description");
 
 
 srcButton.addEventListener("click" , function(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q={cityName.value}&appid={db8228f3d79f00371f8b536941d70ec8}")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&APPID=37371c4f418dee06ea0a0e9daeeebd12")
     .then( response => response.json())
     .then( data  => console.log(data))
     .catch(err => alert("wrong city name"))
 
 })
 
-// fetch ("api.openweathermap.org/data/2.5/weather?q={cityName}&appid={37371c4f418dee06ea0a0e9daeeebd12}")
+// fetch("api.openweathermap.org/data/2.5/weather?q={cityName}&appid={37371c4f418dee06ea0a0e9daeeebd12}")
 // .then( response => response.json())
 // .than(data => )
+
+
+// srcButton.addEventListener("click" , function(){
+//     fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=37371c4f418dee06ea0a0e9daeeebd12")
+//     .then( response => response.json())
+//     .then( data  => console.log(data))
+//     .catch(err => alert("wrong city name"))
+
+// })
