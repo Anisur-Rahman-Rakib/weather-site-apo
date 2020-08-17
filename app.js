76e7b87efcbb00d1c4c779c3fe82b0bfa83f1747
Anisur-Rahman-Rakib <amianis1234567890@gmail.com>
2020-08-17 11:44:41 +0600

    weather app done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let srcButton = document.getElementById("search-button");
 
 let inputValue = document.getElementById("input-value");
 
+
+
 let cityName = document.getElementById("city-name");
 
 let temperatureValue = document.getElementById("temperature");
@@ -12,10 +14,38 @@ let descriptionText = document.getElementById("description");
 srcButton.addEventListener("click" , function(){
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&APPID=37371c4f418dee06ea0a0e9daeeebd12")
     .then( response => response.json())
-    .then( data  => console.log(data))
-    .catch(err => alert("wrong city name"))
+    // .then( data  => console.log(data))
+    .then(data => {
+   
+        let nameValue = data['name'];
+        let tempValue = data['main']['temp'];
+        let descValue = data['weather'][0]['description'];
+            
+      
+       cityName.innerText = nameValue;
+       temperatureValue.innerText = tempValue;
+       descriptionText.innerText = descValue;
+    })
 
 })
+
+
+// try any way.........
+
+
+
+
+// let cloudIcon = data['icon];
+    //     let nameValue = data['name'];
+    //     let tempValue = data['main']['temp'];
+    //     let descValue = data['weather']['description'];
+            
+    // cloudImg.value =cloudIcon;
+    //    cityName.innerText = nameValue;
+    //  temperatureValue.innerText = tempValue;
+    //    descriptionTex.innerText = descValue;
+        
+    // })
 
 // fetch("api.openweathermap.org/data/2.5/weather?q={cityName}&appid={37371c4f418dee06ea0a0e9daeeebd12}")
 // .then( response => response.json())
@@ -29,3 +59,5 @@ srcButton.addEventListener("click" , function(){
 //     .catch(err => alert("wrong city name"))
 
 // })
+ // .then( data  => console.log(data))
+    // .catch(err => alert("wrong city name"))
